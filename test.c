aa7014b3ba8fb0b38a195550de0249af09d423ad
KuroNeko <newez2dj7@naver.com>
2017-08-14 14:43:19 +0900

    interface make file

--- test.c
+++ test.c
@@ -29,23 +29,21 @@ typedef struct arphdr {
 	uint8_t tpa[4];      /* Target IP address       */ 
 }arphdr_t; 
 
-typedef struct ip{
-	uint8_t hdr_len:4;
-	uint8_t version:4;
+typedef struct iphdr
+{
+	uint32_t ihl : 4;
+	uint32_t version : 4;
 	uint8_t tos;
-	uint16_t total_len;
+	uint16_t tot_len;
 	uint16_t id;
-	uint8_t ip_frag_offset:5;
-	uint8_t ip_more_fragment:1;
-	uint8_t ip_dont_fragment:1;
-	uint8_t ip_reserved_zero:1;
-	uint8_t ip_frag_offset1;
-	uint8_t ip_ttl;
-	uint8_t ip_protocol;
-	uint16_t ip_checksum;
-	struct in_addr ip_srcaddr;
-	struct in_addr ip_destaddr;
+	uint16_t frag_off;
+	uint8_t ttl;
+	uint8_t protocol;
+	uint16_t check;
+	uint32_t saddr;
+	uint32_t daddr;
 } ip;
+
 
 typedef struct tcp{
 	uint16_t source_port;
@@ -73,49 +71,52 @@ typedef struct tcp{
 
 #define MAC_SIZE	6
 
-typedef struct arp_packet {
+#pragma pack(push, 1)
+typedef struct packet {
 	ethernet ether;
-	arphdr_t arp;
-} arp_packet;
+	ip iph;
+	tcp tcph;
+} _packet;
+#pragma pack(pop)
+
+#define CLASS_CREATE 520
+#define CLASS_DELETE 514
+#define CLASS_TRIGER 512
+#define CLASS_NOPNOP 9505
+
+uint8_t *inter_destmac[6] = { "\x00\x0c\x0f\x14\x15\x17", "\x3c\x14\x1f\x00\x41\x42", "\xfc\xf0\x1f\x47\x44\x08" };
+uint32_t inter_destaddr[3] = { 0x64008080, 0x1f1e8080, 0x65648080 };
+uint32_t inter_destport[3] = { CLASS_NOPNOP, CLASS_DELETE, CLASS_CREATE };
+uint8_t *inter_filename[3] = { "inter0.pcap\x00", "inter1.pcap\x00", "inter2.pcap\x00" };
+
+typedef struct interface {
+	uint8_t  dest_mac[MAC_SIZE];
+	uint32_t dest_addr;
+	uint32_t dest_port;
+}interface;
 
 int main(int argc, char *argv[]){
-	arp_packet packet;
+	_packet packet;
 
-	// 0,0Ch,0Fh,14h,15h,17
-	memcpy(packet.ether.dest, "\x00\x0c\x0f\x14\x15\x17", MAC_SIZE);
-	memcpy(packet.ether.src , "\xff\xff\xff\xff\xff\xff", MAC_SIZE);
-	packet.ether.type = htons(ETHERTYPE_ARP);
+	interface inter[3];
 
-	/*
-	   typedef struct arphdr {
- 		    uint16_t htype;
-			uint16_t ptype;
-			uint8_t hlen; 
-			uint8_t plen; 
-			uint16_t oper;          
-			uint8_t sha[6];
-			uint8_t spa[4];
-			uint8_t tha[6];
-			uint8_t tpa[4];
-		}arphdr_t;
-	 */
+	for(int i = 0; i < 3; i++){
+		memcpy(inter[i].dest_mac, inter_destmac[i], MAC_SIZE);
+		inter[i].dest_addr = inter_destaddr[i];
+		inter[i].dest_port = inter_destport[i];
 
-	packet.arp.htype = htons(1);
-	packet.arp.ptype = htons(ETHERTYPE_IP);
-	packet.arp.hlen = 6;
-	packet.arp.plen = 4;
-	packet.arp.oper = htons(ARP_REQUEST);
+		memcpy(packet.ether.dest, inter[i].dest_mac, MAC_SIZE);
+		memcpy(packet.ether.src, "\xff\xff\xff\xff\xff\xff", MAC_SIZE);
+		packet.ether.type = htons(ETHERTYPE_IP);
 
-	memcpy(packet.arp.sha, packet.ether.src, ETHER_ADDR_LEN);
-	memcpy(packet.arp.spa, "\x95\x05\x95\x05", 4);
-	memset(packet.arp.tha, 0x00, ETHER_ADDR_LEN);
-	memcpy(packet.arp.tpa, "\x00\x00\x00\x00", 4);
-
-	FILE *fp = fopen("./packet.pcap", "wb");
-
-	fwrite(&packet, 1, sizeof(packet), fp);
-
-	fclose(fp);
+		packet.iph.daddr = inter[i].dest_addr;
+		packet.iph.saddr = inter[i].dest_addr;
+		packet.tcph.dest_port = htons(inter[i].dest_port);
+	
+		FILE *fp = fopen(inter_filename[i], "wb");
+		fwrite(&packet, 1, sizeof(packet), fp);
+		fclose(fp);
+	}
 
 	return 0;
 }
